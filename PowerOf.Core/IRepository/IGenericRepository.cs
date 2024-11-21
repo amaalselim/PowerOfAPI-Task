10d5d523ec53cff4a6778d914a17ee434ba36a01
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-21 17:27:34 +0200

    create Generic Repository Interface

---
+++ PowerOf.Core/IRepository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task AddEntityAsync(T entity);
+        Task<IEnumerable<T>> GetAllEntityAsync();
+        Task<T?> GetEntityByIdAsync(string id);
+        Task UpdateEntityAsync(T entity);
+        Task DeleteEntityAsync(string id);
+    }
+}
