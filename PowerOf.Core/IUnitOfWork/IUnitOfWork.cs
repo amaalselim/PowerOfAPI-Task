7aae7563eca4647ca492a12d8783f5d1d161cd07
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-21 17:28:44 +0200

    Create IUnitOfWork

---
+++ PowerOf.Core/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using PowerOf.Core.Entities;
+using PowerOf.Core.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IUnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Product> _ProductRepository { get; }
+        Task<int> Complete();
+
+    }
+}
