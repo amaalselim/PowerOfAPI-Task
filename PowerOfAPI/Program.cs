c93a4e3a0d15f4056b2a463ffdc25f399043bd4c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-21 17:27:19 +0200

    add basics

--- PowerOfAPI/Program.cs
+++ PowerOfAPI/Program.cs
@@ -1,3 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PowerOf.Core.Entities;
+using PowerOf.Core.IRepository;
+using PowerOf.Core.IUnitOfWork;
+using PowerOf.Persistence.Data;
+using PowerOf.Persistence.Repository;
+using PowerOf.Persistence.UnitOfWork;
 
 namespace PowerOfAPI
 {
@@ -6,13 +16,70 @@ namespace PowerOfAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
+
+            builder.Services.AddIdentity<User, IdentityRole>()
+                           .AddEntityFrameworkStores<ApplicationDbContext>()
+                           .AddDefaultTokenProviders();
+
+            builder.Services.AddScoped<IGenericRepository<Product>, ProductRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            builder.Services.AddCors(corsOptions =>
+            {
+                corsOptions.AddPolicy("MyPolicy", policyBuilder =>
+                {
+                    policyBuilder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+                });
+            });
+
+
+
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+
+            /*-----------------------------Swagger Part-----------------------------*/
+            #region Swagger REgion
+
+            // إضافة خدمات Swagger
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Laza API", Version = "v1" });
+
+                // Add support for bearer token authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter 'Bearer' followed by a space and the token.",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+            });
+
+            #endregion
 
             var app = builder.Build();
 
@@ -23,6 +90,10 @@ namespace PowerOfAPI
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("MyPolicy");
+
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
