4eec4d178fc635724bf3b9eacc421fdef0f28ce8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:01:11 +0200

    create IproductRepo

---
+++ PowerOf.Core/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PowerOf.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IRepository
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<IEnumerable<Product>> SearchProductAsync(string searchTerm);
+    }
+}
