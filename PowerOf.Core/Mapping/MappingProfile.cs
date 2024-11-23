eb0e190ce65112bad396f6f01815bee007a4b8bb
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:01:49 +0200

    Auto Mapping between models and objects

---
+++ PowerOf.Core/Mapping/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PowerOf.Application.DTO_s;
+using PowerOf.Core.DTO_s;
+using PowerOf.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Service, ServiceDTO>().ReverseMap();
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDTO>().ReverseMap();
+            CreateMap<RegisterDTO, User>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
+
+            CreateMap<Invoice,InvoiceDTO>().ReverseMap();   
+
+        }
+    }
+}
