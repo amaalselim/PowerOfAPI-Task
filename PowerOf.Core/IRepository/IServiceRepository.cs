1e3f63ee2ed7f763e22c1de4698577aa22af55ad
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:01:23 +0200

    Create IserviceRepo

---
+++ PowerOf.Core/IRepository/IServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using PowerOf.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IRepository
+{
+     public interface IServiceRepository : IGenericRepository<Service>
+     {
+        Task<List<Service>> SearchServicesAsync(string name, decimal? price);
+     }
+}
