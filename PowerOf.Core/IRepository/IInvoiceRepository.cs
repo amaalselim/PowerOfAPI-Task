da8550ae3e200b6aa553a172d8cffd2958926bee
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:00:48 +0200

    create IInvoice repo

---
+++ PowerOf.Core/IRepository/IInvoiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using PowerOf.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IRepository
+{
+    public interface IInvoiceRepository
+    {
+        Task<Invoice> CreateInvoiceAsync(int orderId);
+        Task<IEnumerable<Invoice>> GetInvoicesByUserIdAsync(string userId);
+        Task<Invoice> GetInvoiceByIdAsync(int invoiceId);
+    }
+}
