916b84f934a0d7b3387af11db1456f75bad62934
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:01:00 +0200

    create Iorder repo

---
+++ PowerOf.Core/IRepository/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using PowerOf.Application.DTO_s;
+using PowerOf.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf.Core.IRepository
+{
+    public interface IOrderRepository
+    {
+        Task AddToOrderAsync(string UserId, OrderItemDTO OrderItemDTO);
+        Task<OrderDTO> GetOrderAsync(string UserId);
+        Task<IEnumerable<Order>> GetOrdersByUserIdAsync(string userId);
+        Task CancelOrderAsync(int orderId);
+        Task<bool> ApproveOrderAsync(int orderId);
+
+    }
+}
