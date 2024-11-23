e1ab159dbcfc37c968b7ed644e13881ee2d6e650
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-23 21:02:35 +0200

    implement UnitOfWork

--- PowerOf.Persistence/UnitOfWork/UnitOfWork.cs
+++ PowerOf.Persistence/UnitOfWork/UnitOfWork.cs
@@ -13,15 +13,32 @@ namespace PowerOf.Persistence.UnitOfWork
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _context;
+        
 
-        public IGenericRepository<Product> _ProductRepository {  get; private set; }
-        public UnitOfWork(ApplicationDbContext context,IGenericRepository<Product> product)
+        public IProductRepository _ProductRepository {  get; private set; }
+
+        public IServiceRepository _ServiceRepository { get; private set; }
+
+        public IOrderRepository _orderRepository { get; private set; }
+
+        public IInvoiceRepository _invoiceRepository {  get; private set; }
+
+        public UnitOfWork(
+            ApplicationDbContext context,
+            IProductRepository product,
+            IServiceRepository service,
+            IOrderRepository order,
+            IInvoiceRepository invoice
+            )
         {
             _context = context;
             _ProductRepository = product;
+            _ServiceRepository = service;
+            _orderRepository = order;
+            _invoiceRepository = invoice;
         }
 
-        public async Task<int> Complete()
+        public async Task<int> CompleteAsync()
         {
             return await _context.SaveChangesAsync();
         }
